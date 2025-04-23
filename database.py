0ccd681cdd17ccbd25b94e4ef3df01034f18596b
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-23 10:37:29 +0800

    s

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
-# app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL = "postgresql://etodoback_user:UZoTXz4WRZKZ0Ql45G7BthrNnworji8y@dpg-d03p003uibrs73agbra0-a.singapore-postgres.render.com/etodoback"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+database_url = os.environ.get("DATABASE_URL")
+engine = create_engine(database_url, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
