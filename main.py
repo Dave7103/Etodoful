d7b7e0facd921e74aab7fa7b9900b4ccc2e6f7b3
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-23 10:19:37 +0800

    s

--- main.py
+++ main.py
@@ -12,13 +12,10 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# CORS settings
-origins = ["*"]
-
 # CORS (for React frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Replace with frontend URL in prod
+    allow_origins=["*"],  # Replace with frontend URL in prod
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
